f123dca83024760b3828c43ba3c0cf63a01e588c
Jhonny <jhonnythomazz@gmail.com>
2025-04-23 13:42:57 -0300

    Update EX_01.java

--- EX_01.java
+++ EX_01.java
@@ -10,3 +10,26 @@ public class EX_01 {
 	}
 
 }
+// i		i<=20		saída
+
+// 1		1<=20(v)	eu sou um jedi em java!
+// 2		2<=20(v)	eu sou um jedi em java!
+// 3		3<=20(v)	eu sou um jedi em java!
+// 4		4<=20(v)	eu sou um jedi em java!
+// 5		5<=20(v)	eu sou um jedi em java!
+// 6		6<=20(v)	eu sou um jedi em java!
+// 7		7<=20(v)	eu sou um jedi em java!
+// 8		8<=20(v)	eu sou um jedi em java!
+// 9		9<=20(v)	eu sou um jedi em java!
+// 10		10<=20(v)	eu sou um jedi em java!
+// 11		11<=20(v)	eu sou um jedi em java!
+// 12		12<=20(v)	eu sou um jedi em java!
+// 13		13<=20(v)	eu sou um jedi em java!
+// 14		14<=20(v)	eu sou um jedi em java!
+// 15		15<=20(v)	eu sou um jedi em java!
+// 16		16<=20(v)	eu sou um jedi em java!
+// 17		17<=20(v)	eu sou um jedi em java!
+// 18		18<=20(v)	eu sou um jedi em java!
+// 19		19<=20(v)	eu sou um jedi em java!
+// 20		20<=20(v)	eu sou um jedi em java!
+// 21		21<=20(f)>	
